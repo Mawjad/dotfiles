1f921dc013206bfe3f005ca5f22506465dc50f59
Mλw <75197841+Mawjad@users.noreply.github.com>
2024-05-15 14:57:54 -0700

    add git lol

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
     environment.systemPackages = with pkgs; [
+      git
       xclip
       gparted
       parted
